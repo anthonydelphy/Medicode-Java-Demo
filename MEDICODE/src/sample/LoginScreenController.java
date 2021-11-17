37a539aa6cb6bba4137df5a8dcc1136761a414cf
anthonydelphy <73381837+anthonydelphy@users.noreply.github.com>
2021-11-17 12:09:47 -0700

    Some log in functionality & PatientInitialController added
    Logs the user in but does not take note of where they are in their respective array.

--- MEDICODE/src/sample/LoginScreenController.java
+++ MEDICODE/src/sample/LoginScreenController.java
@@ -7,6 +7,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -56,19 +57,41 @@ public class LoginScreenController {
 
     public void checkType(ActionEvent event) throws IOException{
         //If Doctor
-        switchToDoctor(event);
+        Database temp = new Database();
+        try {
+            temp = ChangeSceneClass.getDatabase(event);
+        }catch(IOException e){e.printStackTrace();}
 
-        /*If Nurse
-        switchToNurse(event);
+        for(int i = 0; i < temp.getDoctorsList().size(); i++){
+            if (loginUsername.getText().equals(temp.getDoctorsList().get(i).getUsername()) && loginPassword.getText().equals(temp.getDoctorsList().get(i).getPassword())) {
+                switchToDoctor(event);
+            }
+        }
+
+
+        //If Nurse
+        for(int i = 0; i < temp.getNursesList().size(); i++){
+            if (loginUsername.getText().equals(temp.getNursesList().get(i).getUsername()) && loginPassword.getText().equals(temp.getNursesList().get(i).getPassword())) {
+                switchToNurse(event);
+            }
+        }
+
 
         //If Patient
-        switchToPatient(event);*/
+        for(int i = 0; i < temp.getPatientList().size(); i++){
+            if (loginUsername.getText().equals(temp.getPatientList().get(i).getUsername()) && loginPassword.getText().equals(temp.getPatientList().get(i).getPassword())) {
+                switchToPatient(event);
+            }
+        }
+
+        return;
 
     }
 
     public void switchToDoctor(ActionEvent event) throws IOException {
-        Main m = new Main();
-        m.changeScene("DoctorInitial.fxml");
+        //Main m = new Main();
+        //m.changeScene("DoctorInitial.fxml");
+        ChangeSceneClass.changeScene(event, "DoctorInitial.fxml");
         /*Parent root = FXMLLoader.load(getClass().getResource("DoctorInitial.fxml"));
         stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
@@ -76,22 +99,29 @@ public class LoginScreenController {
         stage.show();*/
 
     }
-    /*
+
     public void switchToNurse(ActionEvent event) throws IOException{
-        Parent root = FXMLLoader.load(getClass().getResource("2NurseSplash.fxml"));
+        ChangeSceneClass.changeScene(event, "NurseInitial.fxml");
+       /* Parent root = FXMLLoader.load(getClass().getResource("2NurseSplash.fxml"));
         stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+        */
+
     }
     public void switchToPatient(ActionEvent event) throws IOException{
+        ChangeSceneClass.changeScene(event, "PatientInitial.fxml");
+        /*
         Parent root = FXMLLoader.load(getClass().getResource("2PatientSplash.fxml"));
         stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+        */
 
-    }*/
+
+    }
 
 
 
